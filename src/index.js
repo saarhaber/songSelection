33319367c1b81891760e18f8be2c2e0ddd37cf17
saarhaber <saarhaber@gmail.com>
2019-08-02 23:20:28 -0400

    ds

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './components/App';
 import reducers from './reducers'
 
-ReactDOM.render(
+ReactDOM.render( <Router basename = "/songSelection">
     <Provider store={createStore(reducers)}>
         <App/>
       </Provider>
+      </Router>
 , document.querySelector('#root')
 );
